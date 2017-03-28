d75697a07ef1e02de313d6364b06fd1fd8c71477
Egor Voronyansky <txn.nastya@gmail.com>
2017-03-28 23:17:01 +0400

    hello worlds yandex maps api

--- chapter5/js/index.js
+++ chapter5/js/index.js
@@ -1,5 +1,6 @@
 window.onload = showData;
-
+//ymaps.ready(showData);
+   var myMap;
 function showData() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -11,6 +12,13 @@ function showData() {
 function showPosition(position) {
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
-
   document.getElementById("location").innerHTML = "You are at latitude: " + latitude + " and longitude: " + longitude;
+  showMap(position.coords);
+}
+
+function showMap(coords) {
+  myMap = new ymaps.Map("map", {
+            center: [coords.latitude, coords.longitude],
+            zoom: 15
+        });
 }
